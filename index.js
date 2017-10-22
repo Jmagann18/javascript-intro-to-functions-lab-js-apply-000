8880dbfe73bc918ca1fbc51bb76890fcaddd760a
john magann <jmagann28@gmail.com>
2017-10-22 21:04:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  string === string.toLowerCase()
-  return "I can't hear you!"
-}
-function sayHiToGrandma(string){
-  string === string.toUpperCase()
-  return "YES INDEED!"
+  if(string === string.toLowerCase()) {
+      return "I can't hear you!"
+  } else if(string === string.toUpperCase()){
+      return "YES INDEED!"
+  }  
 }
