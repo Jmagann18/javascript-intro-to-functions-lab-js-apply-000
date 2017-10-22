5db9969a95673e73ce88e6f27786eff52ccbb05e
john magann <jmagann28@gmail.com>
2017-10-22 20:42:11 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -27,7 +27,7 @@ describe('logWhisper(string)', function() {
   it('calls console.log() its one argument in all lowercase', function() {
     const spy = expect.spyOn(console, 'log').andCallThrough()
 
-    logWhisper('hello')
+    logWhisper('HELLO')
 
     expect(spy).toHaveBeenCalledWith('hello')
 
